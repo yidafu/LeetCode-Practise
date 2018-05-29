edfae16e04a776e61786a2afcaeee8224c01d03e
yidafu <yidafu90@qq.com>
2018-05-29 22:24:34 +0800

    timeout

---
+++ LongestPalindromicSubstring/LongestPalindromicSubstring.rb
@@ -0,0 +1,29 @@
+def symmetry?(s)
+    (s.length/2).times do |index|
+        if s[index] != s[s.length - 1 - index]
+            return false
+        end
+    end
+    return true
+end
+
+# @param {String} s
+# @return {String}
+def longest_palindrome(s)
+    found = ''
+    for step in 0..(s.length)
+        s.length.times do |index|
+            if s[index] == s[index + step]
+                tmp = s[index..(index+step)]
+                if symmetry?(tmp)
+                    found = tmp
+                    break
+                end
+            end
+        end
+    end
+    puts found
+    return found
+end
+
+longest_palindrome "esbtzjaaijqkgmtaajpsdfiqtvxsgfvijpxrvxgfumsuprzlyvhclgkhccmcnquukivlpnjlfteljvykbddtrpmxzcrdqinsnlsteonhcegtkoszzonkwjevlasgjlcquzuhdmmkhfniozhuphcfkeobturbuoefhmtgcvhlsezvkpgfebbdbhiuwdcftenihseorykdguoqotqyscwymtjejpdzqepjkadtftzwebxwyuqwyeegwxhroaaymusddwnjkvsvrwwsmolmidoybsotaqufhepinkkxicvzrgbgsarmizugbvtzfxghkhthzpuetufqvigmyhmlsgfaaqmmlblxbqxpluhaawqkdluwfirfngbhdkjjyfsxglsnakskcbsyafqpwmwmoxjwlhjduayqyzmpkmrjhbqyhongfdxmuwaqgjkcpatgbrqdllbzodnrifvhcfvgbixbwywanivsdjnbrgskyifgvksadvgzzzuogzcukskjxbohofdimkmyqypyuexypwnjlrfpbtkqyngvxjcwvngmilgwbpcsseoywetatfjijsbcekaixvqreelnlmdonknmxerjjhvmqiztsgjkijjtcyetuygqgsikxctvpxrqtuhxreidhwcklkkjayvqdzqqapgdqaapefzjfngdvjsiiivnkfimqkkucltgavwlakcfyhnpgmqxgfyjziliyqhugphhjtlllgtlcsibfdktzhcfuallqlonbsgyyvvyarvaxmchtyrtkgekkmhejwvsuumhcfcyncgeqtltfmhtlsfswaqpmwpjwgvksvazhwyrzwhyjjdbphhjcmurdcgtbvpkhbkpirhysrpcrntetacyfvgjivhaxgpqhbjahruuejdmaghoaquhiafjqaionbrjbjksxaezosxqmncejjptcksnoq"
